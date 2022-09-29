e7b3b139a8ae9e25404242a41a423b68b1c30f52
tparfenov <tparfenov@rbc.ru>
2022-09-29 18:57:24 +0300

    files added

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
       };
     in {
 
-        nixosConfigurations = nixpkgs.lib.nixosSystem {
+        nixosConfigurations.myhost = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux"; 
           specialArgs = {
             inherit inputs;
