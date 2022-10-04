d8a2bb7b2d379586d04a815cbd734dc0ebb38941
agud97 <agud97@mail.ru>
2022-10-04 20:41:43 +0000

    conf-n.nix updated

--- configuration.nix
+++ configuration.nix
@@ -33,10 +33,11 @@ in {
   ];
 };
   
-  imports = [ /etc/nixos/hardware-configuration.nix 
+  imports = [ #./amazon-image.nix
+  "${modulesPath}/virtualisation/amazon-image.nix"
 		./sl-graph.nix
    ];
- # ec2.hvm = true;
+  ec2.hvm = true;
 
 #environment.systemPackages = with pkgs; [ 
 #  vim 
@@ -63,25 +64,35 @@ nix = {
   '';
 };
 
-services.openssh.enable = true;
-services.openssh.permitRootLogin = "yes";
 services.sl-graph.enable = true;
 #inputs.slgraph.within.services.sl-graph = {
 #    enable = true;
 #  };
   services.nginx = {
     enable = true;
-    virtualHosts.${domain}.locations."/" = {
+    virtualHosts."_".locations."/api" = {
+     extraConfig = '' 
+       try_files $uri /index.php$is_args$args; 
+     '';
+    };
+    virtualHosts."_".locations."/" = {
+      root = "${inputs.slweb.packages.${pkgs.system}.default}/lib/node_modules/sl-web-config/dist";
+     extraConfig = '' 
+      index  index.html;
+      try_files $uri $uri/ /index.html;
+     '';
+    };
+    virtualHosts."_".locations."~ ^/index\.php(/|$)" = {
 #      root = "${inputs.slweb.packages.${pkgs.system}.default}/lib/node_modules/sl-web-config/dist";
       root = inputs.sourcecode; 
    #  root = my-source.source-code;
       extraConfig = ''
-        #fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        #fastcgi_pass unix:${config.services.phpfpm.pools.${app}.socket};
-        #fastcgi_param DOCUMENT_ROOT $realpath_root;
-        #fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
-        #include ${pkgs.nginx}/conf/fastcgi_params;
-        #include ${pkgs.nginx}/conf/fastcgi.conf;
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass unix:${config.services.phpfpm.pools.${app}.socket};
+        fastcgi_param DOCUMENT_ROOT $realpath_root;
+        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
+        include ${pkgs.nginx}/conf/fastcgi_params;
+        include ${pkgs.nginx}/conf/fastcgi.conf;
         #fastcgi_param     SCRIPT_FILENAME /srv/http/phpdemo.example.com/index.php;
         #fastcgi_param     SCRIPT_NAME          /index.php;
       '';
