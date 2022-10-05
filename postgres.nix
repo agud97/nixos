d023e0c397b9a992c55b5cc0d685323bf04179de
agud97 <agud97@mail.ru>
2022-10-05 08:12:20 +0000

    nginx.nix postgres.nix

--- postgres.nix
+++ postgres.nix
@@ -5,37 +5,6 @@ let
   app = "phpdemo";
   domain = "${app}.example.com";
 in {
-  services.nginx = {
-    enable = true;
-    virtualHosts."_".locations."/api" = {
-     extraConfig = '' 
-       try_files $uri /index.php$is_args$args; 
-     '';
-    };
-    virtualHosts."_".locations."/" = {
-      root = "${inputs.slweb.packages.${pkgs.system}.default}/lib/node_modules/sl-web-config/dist";
-     extraConfig = '' 
-      index  index.html;
-      try_files $uri $uri/ /index.html;
-     '';
-    };
-    virtualHosts."_".locations."~ ^/index\.php(/|$)" = {
-#      root = "${inputs.slweb.packages.${pkgs.system}.default}/lib/node_modules/sl-web-config/dist";
-      root = inputs.sourcecode; 
-   #  root = my-source.source-code;
-      extraConfig = ''
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass unix:${config.services.phpfpm.pools.${app}.socket};
-        fastcgi_param DOCUMENT_ROOT $realpath_root;
-        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
-        include ${pkgs.nginx}/conf/fastcgi_params;
-        include ${pkgs.nginx}/conf/fastcgi.conf;
-        #fastcgi_param     SCRIPT_FILENAME /srv/http/phpdemo.example.com/index.php;
-        #fastcgi_param     SCRIPT_NAME          /index.php;
-      '';
-     };
-  };
-
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_10;
