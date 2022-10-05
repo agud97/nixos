e833e8b9d12cfede567dea4e2f7800d3e9583069
agud97 <agud97@mail.ru>
2022-10-05 08:01:05 +0000

    nginx.nix postgres.nix

--- configuration.nix
+++ configuration.nix
@@ -21,6 +21,8 @@ in {
                # /etc/nixos/hardware-configuration.nix 
 		./sl-graph.nix
                 ./php-fpm.nix
+                ./nginx.nix
+                ./postgres.nix
    ];
   ec2.hvm = true;
 
@@ -55,36 +57,6 @@ services.sl-graph.enable = true;
 #inputs.slgraph.within.services.sl-graph = {
 #    enable = true;
 #  };
-  services.nginx = {
-    enable = true;
-    virtualHosts."_".locations."/api" = {
-     extraConfig = '' 
-       try_files $uri /index.php$is_args$args; 
-     '';
-    };
-    virtualHosts."_".locations."/" = {
-      root = "${inputs.slweb.packages.${pkgs.system}.default}/lib/node_modules/sl-web-config/dist";
-     extraConfig = '' 
-      index  index.html;
-      try_files $uri $uri/ /index.html;
-     '';
-    };
-    virtualHosts."_".locations."~ ^/index\.php(/|$)" = {
-#      root = "${inputs.slweb.packages.${pkgs.system}.default}/lib/node_modules/sl-web-config/dist";
-      root = inputs.sourcecode; 
-   #  root = my-source.source-code;
-      extraConfig = ''
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
-        fastcgi_pass unix:${config.services.phpfpm.pools.${app}.socket};
-        fastcgi_param DOCUMENT_ROOT $realpath_root;
-        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
-        include ${pkgs.nginx}/conf/fastcgi_params;
-        include ${pkgs.nginx}/conf/fastcgi.conf;
-        #fastcgi_param     SCRIPT_FILENAME /srv/http/phpdemo.example.com/index.php;
-        #fastcgi_param     SCRIPT_NAME          /index.php;
-      '';
-     };
-  };
   users.users.${app} = {
     isSystemUser = true;
     createHome = false;
@@ -93,20 +65,5 @@ services.sl-graph.enable = true;
   };
   users.groups.${app} = {};
 
-  services.postgresql = {
-    enable = true;
-    package = pkgs.postgresql_10;
-    enableTCPIP = true;
-    authentication = pkgs.lib.mkOverride 10 ''
-      local all all trust
-      host all all 127.0.0.1/32 trust
-      host all all ::1/128 trust
-    '';
-    initialScript = pkgs.writeText "backend-initScript" ''
-      CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
-      CREATE DATABASE nixcloud;
-      GRANT ALL PRIVILEGES ON DATABASE nixcloud TO nixcloud;
-    '';
-  };
 
 }
